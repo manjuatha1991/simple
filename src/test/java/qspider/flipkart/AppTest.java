db4b4ee9ed080cd360cad9a3da940bd8fa73d405
manju <manju@192.168.3.13>
2020-01-08 21:21:57 +0530

    simplepom

---
+++ src/test/java/qspider/flipkart/AppTest.java
@@ -0,0 +1,22 @@
+package qspider.flipkart;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.testng.Reporter;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+    System.out.println("HELLO WORLD IN WINDOWwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkdddddddddddddddddddddddddddddddkffnkj");
+        assertTrue( true );
+    }
+}
